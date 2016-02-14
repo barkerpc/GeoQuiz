0d3e2eb4f20c48a401116b3d26e3262bd6e70114
Paul Barker <barkerpc@gmail.com>
2016-02-14 13:43:15 -0500

    Chapter 1

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -36,7 +37,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                // Doesnt do anything yet
+                Toast.makeText(QuizActivity.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
             }
          });
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -44,7 +45,7 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                // Doesnt do anything yet
+                Toast.makeText(QuizActivity.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
             }
         });
     }
